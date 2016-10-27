66c3cd974a01a3836903916f0f53866c267dc28e
Cxc <393171302@qq.com>
2016-10-27 18:43:52 +0800

    Add generic.h

---
+++ app_IM/SearchUser/Generic.h
@@ -0,0 +1,15 @@
+// File name : Generic.h
+// Description : 一些常用的宏
+
+#ifndef OS_UTIL_GENERIC_H
+#define OS_UTIL_GENERIC_H
+#define LIST_REQUEST 555 //  请求好友列表
+#define LIST_REQUEST_AGREE 444 // 服务器同意列表请求
+#define FRIEND_REQUEST 666 // 请求添加好友
+#define FRIEND_REQUEST_AGREE 777 //服务器同意好友请求
+#define MAXSIZE 255
+
+#include <stdio.h>
+#include <memory.h>
+
+#endif // OS_UTIL_GENERIC_H

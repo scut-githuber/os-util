0b10dc7f7674296f37b455823a99da91e240ea74
jacklx <liuaxucumt@163.com>
2016-11-09 14:52:19 +0800

    Update viewMessage.h

--- app_IM/viewMessage/viewMessage.h
+++ app_IM/viewMessage/viewMessage.h
@@ -1,1 +1,21 @@
+#ifndef VIEWMESSAGE_H_INCLUDED
+#define VIEWMESSAGE_H_INCLUDED
 
+/**
+ * include head file
+ */
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+
+/**
+ * declare function
+ */
+
+
+extern int PostUsrMsg(Data const *data_enc);
+extern int GetSendRes(int post_res);
+extern int ProcessMsgRes(int send_res);
+#endif // SENDMESSAGE_H_INCLUDED

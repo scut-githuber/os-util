aa68a2bcf7f08bcd43498b5c469f6a4885a052b5
jacklx <liuaxucumt@163.com>
2016-11-09 14:46:54 +0800

    Update view.c

--- app_IM/viewMessage/view.c
+++ app_IM/viewMessage/view.c
@@ -1,1 +1,26 @@
+/*查看聊天记录
+input:username
+output:mess
+*/
+int view(char username[MAX_NAME]) {
+	char mess[MAX_BUF];
+	FILE *cfptr;//文件指针
+	if ((cfptr = fopen(username, "r")) == NULL) {
+		printf("无法找到记录文件\n");
+		fclose(cfptr);
+		return 0;
+	} else {
+		while (!feof(cfptr)) {
 
+			fscanf(cfptr, "%s", mess);
+
+			if ((strcmp(username," ") != 0)) {
+				printf("\t %s", mess);
+			}
+			printf("\n");
+		}
+	}
+
+	fclose(cfptr);
+	return 0;
+}

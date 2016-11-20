601b44cb24ff35446d27dddc7f4efd5e92129e53
ding123456 <ding123456@users.noreply.github.com>
2016-11-20 20:36:09 +0800

    Add files via upload

---
+++ app_Website/pay/pay.c
@@ -0,0 +1,17 @@
+include "pay.h"
+
+
+order_profile * pay(){
+
+/**
+* 调用京东API支付api示例
+
+* DefaultJdClient(SERVER_URL,accessToken,appKey,appSecret);
+* EccPaymentServiceQueryPaymentJournalRequest request=
+* new EccPaymentServiceQueryPaymentJournalRequest();
+* request.setOrderId( 123 );
+* EccPaymentServiceQueryPaymentJournalResponse response=
+* client.execute(request);
+*/
+
+}

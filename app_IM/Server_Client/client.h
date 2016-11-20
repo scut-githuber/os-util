85c9d3242c769e2f12a6ae198408426ed45d476b
530FYZ <2545010896@qq.com>
2016-11-20 19:38:59 +0800

    给client.h和server.h定义的函数接口添加注释

--- app_IM/Server_Client/client.h
+++ app_IM/Server_Client/client.h
@@ -14,10 +14,10 @@
 #include <arpa/inet.h>    /* inet(3) functions */
 #include "../Generic.h"
 
-int handle(int fd);
-extern int CreateClientSocket(const char* address,int Port);
-extern int Send_Client(const char *buff,int fd);
-int Receive_Client(char * buff,int fd);
-int handle(int fd);
+int handle(int fd);//处理来自服务器返回的消息
+extern int CreateClientSocket(const char* address,int Port);//创建socket和服务器通信
+extern int Send_Client(const char *buff,int fd);//向服务器发送请求
+int Receive_Client(char * buff,int fd);//接收来自服务器返回的消息
+int handle(int fd);//处理来自服务器返回的消息
 
 #endif //OS_UTIL_CLIENT_H_H
--- app_IM/Server_Client/server.h
+++ app_IM/Server_Client/server.h
@@ -19,12 +19,12 @@
 #include <unistd.h>
 #include "../Generic.h"
 
-int handle(int point);
-int CreateServerSocket(const char *address,int Port);
-int SocketBind(int sfd);
-int SocketListen(int sfd);
-extern int Receive_Server(char *buff,int fd);
-extern int Send_Server(const char *buff,int fd);
-int handle(int point);
+int handle(int point);//处理从客户端发送来的请求
+int CreateServerSocket(const char *address,int Port);//创建socket通信
+int SocketBind(int sfd);//绑定服务器地址
+int SocketListen(int sfd);//监听来自不同客户端是否有发送请求
+extern int Receive_Server(char *buff,int fd);//接收来自客户端的消息
+extern int Send_Server(const char *buff,int fd);//相应客户端的请求
+int handle(int point);//处理从客户端发送来的请求
 
 #endif //OS_UTIL_SERVER_H_H

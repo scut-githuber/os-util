e839bf36c57c84bbac42f86529139dbe7b74269d
jacklx <liuaxucumt@163.com>
2016-11-21 19:42:34 +0800

    Create searchcontent.h

---
+++ app_IM/SearchContent/searchcontent.h
@@ -0,0 +1,9 @@
+#ifndef OS_UTIL_SEARCHCONTENT_H
+#define OS_UTIL_SEARCHCONTENT_H
+
+#include "../Generic.h"
+#include "../../Server_Client/client.h"
+
+extern string *searchcontent(int fd);
+
+#endif 
